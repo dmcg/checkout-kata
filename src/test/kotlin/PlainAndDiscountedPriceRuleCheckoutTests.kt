9ca8274b98b516866c373449590a2a15f3f8898b
Duncan McGregor <duncan@oneeyedmen.com>
2026-01-04 14:02:30 +0000

    Rename test

--- src/test/kotlin/PlainAndDiscountedPriceRuleCheckoutTests.kt
+++ src/test/kotlin/PlainAndDiscountedPriceRuleCheckoutTests.kt
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class DiscountedPriceRuleCheckoutTests {
+class PlainAndDiscountedPriceRuleCheckoutTests {
 
     private val rules: List<PriceRule> = listOf(
         PlainPriceRule("A", 50),
